056d77e095a0203e542d27f2c20aea696d935aaa
Hiram Cruz <hiram0101@gmail.com>
2018-02-01 12:56:56 -0500

    Added check for running game

--- GoPUBGMaps.go
+++ GoPUBGMaps.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/mitchellh/go-ps"
+	"os"
+)
 
+// TslGame.exe
+
+func checkRunning(processName string) bool {
+	out, _ := ps.Processes()
+	for i := 0; i < len(out); i++ {
+		if processName == out[i].Executable() {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	var PUBG = "TslGame.exe"
+
+	if checkRunning(PUBG) {
+		fmt.Println("Game is currently running. Exiting...")
+		os.Exit(0)
+	}
 }
