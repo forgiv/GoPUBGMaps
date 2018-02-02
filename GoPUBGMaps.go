0a0a716fe06b2d90afb053f557f72acdef422aef
Hiram Cruz <hiram0101@gmail.com>
2018-02-02 04:22:45 -0500

    Added function that confirms a given path Moved executable and folder names to global scope for easier handling

--- GoPUBGMaps.go
+++ GoPUBGMaps.go
@@ -3,7 +3,13 @@ package main
 import (
 	"fmt"
 	"github.com/mitchellh/go-ps"
+	"path"
+	"path/filepath"
+	"os"
 )
+
+const PUBGExe  = "TslGame.exe"
+const GameFolder = "PUBG"
 
 func checkRunning(processName string) bool {
 	out, _ := ps.Processes()
@@ -15,11 +21,29 @@ func checkRunning(processName string) bool {
 	return false
 }
 
-func main() {
-	var PUBG = "TslGame.exe"
+func confirmPath(rootPath string) bool {
+	var correctPath = false
 
-	for checkRunning(PUBG) {
+	err := filepath.Walk(rootPath, func(filePath string, info os.FileInfo, err error) error {
+		if path.Base(filePath) == GameFolder {
+			correctPath = true
+		}
+		return err
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	return correctPath
+}
+
+func main() {
+	var input string
+
+	for checkRunning(PUBGExe) {
 		fmt.Println("Game is currently running.")
-		fmt.Scanln("Exit game and press enter to continue.")
+		fmt.Println("Exit game and press enter to continue.")
+		fmt.Scanln(&input)
 	}
 }
